1bd73871a4d71604c050cbfd4a3eb44faf179b0e
GaanOmy <99938286+GaanPly@users.noreply.github.com>
2026-04-21 20:11:46 +0700

    Create Dosen22.java

---
+++ Jobsheet3/Dosen22.java
@@ -0,0 +1,13 @@
+public class Dosen22 {
+    public String kode;
+    public String nama;
+    public boolean gender;
+    public int usia;
+
+    public Dosen22(String kode, String nama, boolean gender, int usia){
+        this.kode = kode;
+        this.nama = nama;
+        this.gender = gender;
+        this.usia = usia;
+    }
+}
